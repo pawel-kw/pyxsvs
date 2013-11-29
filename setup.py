8df678bb9841d8b5582c1c8ac87fd04ac3182b7a
Pawel Kwasniewski <pawel.kw@gmail.com>
2013-11-29 15:54:57 +0100

    mbcs bug workaround

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from distutils.core import setup
+# Work around mbcs bug in distutils.
+# http://bugs.python.org/issue10945
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func) 
+
 setup(name='pyxsvs',
       version='0.1',
       description='Python X-ray Speckle Visibility analysis tools',
