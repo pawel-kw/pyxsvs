46ffee3b44e419aa506d336411f84d7514dc9ded
Pawel Kwasniewski <pawel.kw@gmail.com>
2014-01-03 16:54:27 +0100

    renamed source dir, dependency problem fixed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except LookupError:
     ascii = codecs.lookup('ascii')
     func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
-    codecs.register(func) 
+    codecs.register(func)
 
 setup(name='pyxsvs',
       version = '0.1.0',
@@ -16,8 +16,9 @@ setup(name='pyxsvs',
       author = 'Pawel Kwasniewski',
       author_email = 'pawel.kw@gmail.com',
       url = 'https://github.com/pawel-kw/pyxsvs',
-      install_requires = ['numpy>=1.7','matplotlib>=1.3','fabio','pyFAI'],
+      install_requires = ['numpy>=1.7','matplotlib>=1.3','pyFAI'],
       packages = ['pyxsvs'],
+      package_dir = {'pyxsvs': 'pyxsvs-src'},
       entry_points = {
           'console_scripts': [
               'pyxsvs = pyxsvs.pyxsvs:main',
@@ -29,3 +30,13 @@ setup(name='pyxsvs',
         '': ['*.txt', '*.rst']
         }
       )
+
+# ###############################################################################
+# Check for Fabio to be present of the system
+# ###############################################################################
+try:
+    import fabio
+except ImportError:
+    print("""pyFAI needs fabIO for all image reading and writing.
+This python module can be found on:
+http://sourceforge.net/projects/fable/files/fabio""")
